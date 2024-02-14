03eb046d6b890eb8fac40d4d84daa8571b2f42f3
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-02-14 02:41:16 +0100

    validate consulting Evaluation

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/EvaluationServiceImpl.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/EvaluationServiceImpl.java
@@ -2,13 +2,14 @@ package fr.ubo.dosi.projectagile.cscievaebackend.services.Impl;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Evaluation;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Rubrique;
 import fr.ubo.dosi.projectagile.cscievaebackend.repository.EvaluationRepository;
+import fr.ubo.dosi.projectagile.cscievaebackend.services.EvaluationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class EvaluationService {
+public class EvaluationServiceImpl implements EvaluationService {
     @Autowired
     private EvaluationRepository er;
 
