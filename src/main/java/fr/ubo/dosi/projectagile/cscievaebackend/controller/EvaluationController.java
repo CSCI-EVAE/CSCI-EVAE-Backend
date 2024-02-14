1da000d110e463712564903663848dd46f77ea1f
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-02-14 11:29:08 +0100

    Changin the api name in the evaluation

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/EvaluationController.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/EvaluationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("evaluation")
+@RequestMapping("/api/v1/admin/evaluation")
 @CrossOrigin(origins = "http://localhost:3000")
 public class EvaluationController {
 
