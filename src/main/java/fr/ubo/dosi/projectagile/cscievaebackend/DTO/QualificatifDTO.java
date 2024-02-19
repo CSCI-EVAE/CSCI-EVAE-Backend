3bea49c4003634ae3c01ccf7add53a55eb6cc5eb
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-02-19 17:32:04 +0100

    adlling get all rubriques and there question with order

---
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/DTO/QualificatifDTO.java
@@ -0,0 +1,7 @@
+package fr.ubo.dosi.projectagile.cscievaebackend.DTO;
+
+public class QualificatifDTO {
+    Integer id;
+    private String maximal;
+    private String minimal;
+}
---
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/mappers/RubriqueMapper.java
@@ -0,0 +1,13 @@
+package fr.ubo.dosi.projectagile.cscievaebackend.mappers;
+
+
+import fr.ubo.dosi.projectagile.cscievaebackend.DTO.RubriqueDTO;
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Rubrique;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface RubriqueMapper {
+    RubriqueDTO rubriqueToRubriqueDTO(Rubrique rubrique);
+    Rubrique rubriqueDTOToRubrique(RubriqueDTO rubriqueDTO);
+
+}
