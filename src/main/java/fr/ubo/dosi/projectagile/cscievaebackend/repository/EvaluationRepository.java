a86aaa7459fb60697a916fc4e09f8dd56bd7bc3c
HamzaCh <hamzach0505@gmail.com>
2024-02-19 22:43:18 +0100

    US6_D LISTER UE

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/repository/EvaluationRepository.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/repository/EvaluationRepository.java
@@ -1,7 +1,17 @@
 package fr.ubo.dosi.projectagile.cscievaebackend.repository;
 
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Enseignant;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+
+        @Query(value = "SELECT * FROM EVALUATION e WHERE e.CODE_UE = :codeUE", nativeQuery = true)
+        List<Evaluation> findByCodeUE(@Param("codeUE") String codeUE);
+        List<Evaluation> findAllByNoEnseignant(Enseignant noEnseignant);
 }
--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/UniteEnseignementServiceImpl.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/UniteEnseignementServiceImpl.java
@@ -1,8 +1,24 @@
 package fr.ubo.dosi.projectagile.cscievaebackend.services.Impl;
 
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Enseignant;
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Evaluation;
+import fr.ubo.dosi.projectagile.cscievaebackend.model.UniteEnseignement;
+import fr.ubo.dosi.projectagile.cscievaebackend.repository.EvaluationRepository;
 import fr.ubo.dosi.projectagile.cscievaebackend.services.UniteEnseignementService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UniteEnseignementServiceImpl implements UniteEnseignementService {
+
+    @Autowired
+    private EvaluationRepository er;
+
+    public List<Evaluation> getUeListe (Enseignant enseignant){
+        return er.findAllByNoEnseignant(enseignant);
+    }
+
 }
