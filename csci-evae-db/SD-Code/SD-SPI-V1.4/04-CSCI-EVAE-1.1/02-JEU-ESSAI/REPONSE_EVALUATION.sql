267c0338578cd996eb0994bc86019f118e83243b
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-02-21 12:39:00 +0100

    Fixing anoma

--- csci-evae-db/SD-Code/SD-SPI-V1.4/04-CSCI-EVAE-1.1/02-JEU-ESSAI/REPONSE_EVALUATION.sql
+++ csci-evae-db/SD-Code/SD-SPI-V1.4/04-CSCI-EVAE-1.1/02-JEU-ESSAI/REPONSE_EVALUATION.sql
@@ -1,6 +1,6 @@
---
---  Creation du jeu d'essai de la table REPONSE_EVALUATION
---
-Insert into REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values (null,'1','21406919','Une pedagogie active tres interessante',null,null);
-Insert into REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values (null,'1','21406961','Cette UE a permis de consolider nos connaissances en BDD',null,null);
-Insert into REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values (null,'1','21406956','Il est tres interessant detre immerge dans une Tierce Maintenance Applicative',null,null);
+REM INSERTING into  REPONSE_EVALUATION
+SET DEFINE OFF;
+Insert into  REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values ('1','1','21406919','Une pedagogie active tres interessante',null,null);
+Insert into  REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values ('2','1','21406961','Cette UE a permis de consolider nos connaissances en BDD',null,null);
+Insert into  REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values ('3','1','21406956','Il est tres interessant detre immerge dans une Tierce Maintenance Applicative',null,null);
+Insert into  REPONSE_EVALUATION (ID_REPONSE_EVALUATION,ID_EVALUATION,NO_ETUDIANT,COMMENTAIRE,NOM,PRENOM) values ('4','3','21406398','Appronfondir nos connaissances en BI est ultime',null,null);
