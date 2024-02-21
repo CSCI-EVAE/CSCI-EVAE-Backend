dd31bcf4bd477ea2f8ca2c4847116ea172c3bf9c
Israe SAIDI <israe.2001.saidi@gmail.com>
2024-02-21 10:39:26 +0100

    soumettre evaluation

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/EvaluationController.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/EvaluationController.java
@@ -3,8 +3,11 @@ package fr.ubo.dosi.projectagile.cscievaebackend.controller;
 
 import fr.ubo.dosi.projectagile.cscievaebackend.DTO.EvaluationDTO;
 import fr.ubo.dosi.projectagile.cscievaebackend.ResponceHandler.ApiResponse;
+import fr.ubo.dosi.projectagile.cscievaebackend.exception.ResourceNotFoundException;
 import fr.ubo.dosi.projectagile.cscievaebackend.mappers.EvaluationMapper;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Evaluation;
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Qualificatif;
+import fr.ubo.dosi.projectagile.cscievaebackend.services.EvaluationService;
 import fr.ubo.dosi.projectagile.cscievaebackend.services.Impl.AuthentificationServiceImpl;
 import fr.ubo.dosi.projectagile.cscievaebackend.services.Impl.EvaluationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +31,28 @@ public class EvaluationController {
     @Autowired
     private EvaluationMapper evaluationMapper;
 
+    @Autowired
+    private EvaluationService evaluationService;
+
     @PreAuthorize("hasAuthority('ENS')")
     @GetMapping("getAll")
     public ApiResponse<Set<Evaluation>> getAll(@AuthenticationPrincipal UserDetails currentUser){
 
         return ApiResponse.ok(as.getAuhtentification(currentUser.getUsername()).getNoEnseignant().getEvaluations());
 
+    }
+
+    @PreAuthorize("hasAuthority('ENS')")
+    @PutMapping("soumettre/{id}")
+    public ApiResponse<EvaluationDTO> soumettreEvaluation(@PathVariable Long id) throws ResourceNotFoundException {
+        try {
+
+            EvaluationDTO updated = evaluationService.updateEvaluation(id);
+            System.out.println("Checking security permissions...");
+            return ApiResponse.ok(updated);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Evaluation not found");
+        }
     }
 
     @PreAuthorize("hasAuthority('ENS')")
