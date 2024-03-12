eaacba8c572784a535d833f5be40ef82edd6004b
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-03-12 11:36:35 +0100

    - get all promotion from formation

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/PromotionController.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/controller/PromotionController.java
@@ -4,7 +4,9 @@ import fr.ubo.dosi.projectagile.cscievaebackend.DTO.PromotionDTO;
 import fr.ubo.dosi.projectagile.cscievaebackend.ResponceHandler.ApiResponse;
 import fr.ubo.dosi.projectagile.cscievaebackend.mappers.PromotionMapper;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Authentification;
+import fr.ubo.dosi.projectagile.cscievaebackend.model.Formation;
 import fr.ubo.dosi.projectagile.cscievaebackend.model.Promotion;
+import fr.ubo.dosi.projectagile.cscievaebackend.repository.FormationRepository;
 import fr.ubo.dosi.projectagile.cscievaebackend.services.Impl.AuthentificationServiceImpl;
 import fr.ubo.dosi.projectagile.cscievaebackend.services.PromotionService;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,11 +28,13 @@ public class PromotionController {
     private final AuthentificationServiceImpl as;
     private final PromotionMapper promotionMapper;
     private final  PromotionService promotionService;
+    private final FormationRepository formationRepository;
 
-    public PromotionController(AuthentificationServiceImpl as, PromotionMapper promotionMapper, PromotionService promotionService) {
+    public PromotionController(AuthentificationServiceImpl as, PromotionMapper promotionMapper, PromotionService promotionService , FormationRepository formationRepository) {
         this.as = as;
         this.promotionMapper = promotionMapper;
         this.promotionService = promotionService;
+        this.formationRepository = formationRepository;
     }
 
     @PreAuthorize("hasAuthority('ENS')")
@@ -47,6 +52,13 @@ public class PromotionController {
     public ResponseEntity<?> getAllPromotions() {
         List<Promotion> promotions = promotionService.getAllPromotions();
         return ApiResponse.ok(promotions);
+    }
+    // get all formation from a given promotion
+    @PreAuthorize("hasAuthority('ENS')")
+    @GetMapping("/formationsForPromotion/{codeformation}")
+    public ResponseEntity<?> getFormationsForPromotion(@PathVariable String codeformation) {
+        Formation formation = formationRepository.findById(codeformation).get();
+        return ApiResponse.ok(formation.getPromotions().stream().map(promotionMapper::promotionToPromotionDTO).collect(Collectors.toSet()));
     }
 
 }
