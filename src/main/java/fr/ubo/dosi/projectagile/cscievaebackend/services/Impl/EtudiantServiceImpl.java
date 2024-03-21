cfc84b03e883401b75e37a948b2f721a625036bc
Mohamed EL HADDADI <mohamed.elhaddadi2@etu.uae.ac.ma>
2024-03-21 11:47:46 +0100

    ajouter etudiant

--- src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/EtudiantServiceImpl.java
+++ src/main/java/fr/ubo/dosi/projectagile/cscievaebackend/services/Impl/EtudiantServiceImpl.java
@@ -72,9 +72,6 @@ public class EtudiantServiceImpl implements EtudiantService {
         userService.registerNewUser(authentification);
         Etudiant etudiant = etudiantMapper.etudiantDTOToEtudiant(etudiantDTO);
         Promotion promotion = promotionRepository.findByPromotionId(etudiantDTO.getCodeFormation(), etudiantDTO.getAnneeUniversitaire());
-        if (promotion.getEtudiants().size() >= promotion.getNbMaxEtudiant()) {
-            throw new IllegalArgumentException("Impossible d'ajouter l'étudiant. La promotion est pleine.");
-        }
         etudiant.setPromotion(promotion);
         Etudiant etu = etudiantRepository.save(etudiant);
         Authentification auth = authentificationRepository.findByLoginConnection(login);
